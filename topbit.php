a00012c89d43018e2208d6888b97e4ff91416c1e
ayerst9198 <78511779+ayerst9198@users.noreply.github.com>
2021-06-23 13:01:48 +1200

    cont

---
+++ topbit.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Book Reviews">
+    <meta name="keywords" content="Books, reading, fiction, non-fiction, genre, reviews">
+    <meta name="author" content="Toby Ayers">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title> Some Books With Reviews</title>
+    
+    <!-- Edit the link below / replace with your chosen google font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet"> 
+    
+    <!-- Edit the name of your style sheet - 'foo' is not a valid name!! -->
+    <link rel="stylesheet" href="css/no_side.css"> 
+    
+</head>
+    
+<body>
+    
+    <div class="wrapper">
+    
+
+        
+        <div class="box banner">
+            
+            <h1>Orchid Reading</h1>
+        </div>    <!-- / banner -->
+
+        <div class="box side">
+
+        <h2>Search | <a class="side" href="showall">Show All</a></h2>
+
+        <i>Type part of the title / author name if desired</i>
+
+        <hr/>
+
+        Title Search<br />
+        Author Search<br />
+        Genre Search<br />
+        Rating Search<br />
+
+
+        </div> <!-- / side bar -->
